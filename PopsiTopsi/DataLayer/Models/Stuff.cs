07550453f86cd70ca93b280085ff6798fc459b2c
Sasa Simic <sasasimic009@gmail.com>
2021-12-13 00:32:04 +0100

    Kreirani modeli klasa.

---
+++ PopsiTopsi/DataLayer/Models/Stuff.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Models
+{
+    public class Stuff
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+    }
+}
