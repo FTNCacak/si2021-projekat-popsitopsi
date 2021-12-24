944388541acae6625337abeff3baacdf1889e173
Boris Nedic <nebo2444@gmail.com>
2021-12-24 19:17:26 +0100

    Kreirana BillItemBusiness klasa.

---
+++ PopsiTopsi/BusinessLayer/BillItemBusiness.cs
@@ -0,0 +1,17 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer
+{
+   public class BillItemsBusiness
+    {
+        public readonly BillItemRepository billItemRepository;
+
+        public BillItemsBusiness()
+        {
+            this.billItemRepository = new BillItemRepository();
+        }
+    }
+}
