8a4e8b8ded399220ca1c472bfb90a769958eef4a
Boris Nedic <nebo2444@gmail.com>
2021-12-24 19:13:10 +0100

    Kreirana StuffBusiness klasa.

---
+++ PopsiTopsi/BusinessLayer/StuffBusiness.cs
@@ -0,0 +1,17 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer
+{
+    public class StuffBusiness
+    {
+        public readonly StuffRepository stuffRepository;
+
+        public StuffBusiness()
+        {
+            this.stuffRepository = new StuffRepository();
+        }
+    }
+}
